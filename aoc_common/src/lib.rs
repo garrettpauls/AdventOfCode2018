a4d58312c56fbdaf180adf90c6ca97c4df526ab1
Garrett Pauls <garrett.pauls@gmail.com>
2018-12-01 20:22:48 -0700

    Refactored common functionality into aoc_common crate.

---
+++ aoc_common/src/lib.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use std::io::{Read, Error};
+
+pub fn advent<Input>(
+    parse_input: &Fn(&str) -> Result<Input, String>,
+    part_one: &Fn(&Input) -> Result<String, String>,
+    part_two: &Fn(&Input) -> Result<String, String>,
+) {
+    use std::env::args;
+
+    // first arg is executable name
+    for filename in args().skip(1) {
+        println!("Input: {}", filename);
+
+        let input = File::open(filename)
+            .map_err(|err| format!("failed to open file: {}", err))
+            .and_then(|mut file| read_file_contents_as_string(&mut file)
+                .map_err(|err| format!("failed to read contents of file: {}", err)))
+            .and_then(|input| parse_input(&input));
+
+        match input {
+            Ok(i) => {
+                run_part("part 1", part_one, &i);
+                run_part("part 2", part_two, &i);
+            }
+            Err(e) => println!("  Could not load input because {}", e),
+        }
+
+        println!()
+    }
+
+    fn run_part<Input>(name: &str, part: &Fn(&Input) -> Result<String, String>, input: &Input) {
+        match part(&input) {
+            Ok(answer) => println!("  Answer for {}: {}", name, answer),
+            Err(err) => println!("  Calculation failed for {}: {}", name, err),
+        }
+    }
+}
+
+fn read_file_contents_as_string(file: &mut File) -> Result<String, Error> {
+    let mut content = String::new();
+    match file.read_to_string(&mut content) {
+        Ok(_) => Ok(content),
+        Err(e) => Err(e)
+    }
+}
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,51 +1,6 @@
-use std::fs::File;
-use std::io::Error;
+extern crate aoc_common;
 
-fn advent<Input>(
-    parse_input: &Fn(&str) -> Result<Input, String>,
-    part_one: &Fn(&Input) -> Result<String, String>,
-    part_two: &Fn(&Input) -> Result<String, String>,
-) {
-    use std::env::args;
-
-    // first arg is executable name
-    for filename in args().skip(1) {
-        println!("Input: {}", filename);
-
-        let input = File::open(filename)
-            .map_err(|err| format!("failed to open file: {}", err))
-            .and_then(|mut file| read_file_contents_as_string(&mut file)
-                .map_err(|err| format!("failed to read contents of file: {}", err)))
-            .and_then(|input| parse_input(&input));
-
-        match input {
-            Ok(i) => {
-                run_part("part 1", part_one, &i);
-                run_part("part 2", part_two, &i);
-            }
-            Err(e) => println!("  Could not load input because {}", e),
-        }
-
-        println!()
-    }
-
-    fn run_part<Input>(name: &str, part: &Fn(&Input) -> Result<String, String>, input: &Input) {
-        match part(&input) {
-            Ok(answer) => println!("  Answer for {}: {}", name, answer),
-            Err(err) => println!("  Calculation failed for {}: {}", name, err),
-        }
-    }
-}
-
-fn read_file_contents_as_string(file: &mut File) -> Result<String, Error> {
-    use std::io::Read;
-
-    let mut content = String::new();
-    match file.read_to_string(&mut content) {
-        Ok(_) => Ok(content),
-        Err(e) => Err(e)
-    }
-}
+use aoc_common::advent;
 
 fn main() {
     advent(&parse_input, &part_one, &part_two);
