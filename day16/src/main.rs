e26a7d466d094d59df78767ead8cec0032b059fc
Garrett Pauls <garrett.pauls@gmail.com>
2018-12-16 22:45:15 -0700

    Day 16 - Part 2 - not all op codes resolved to single operation

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -5,17 +5,22 @@ mod ops;
 use aoc_common::read_file_contents_as_string_from_path;
 use ops::*;
 use std::str::FromStr;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     match part_one() {
-        Ok(msg) => println!("Success: {}", msg),
-        Err(err) => println!("Failed: {}", err),
+        Ok(msg) => println!("One Success: {}", msg),
+        Err(err) => println!("One Failed: {}", err),
+    }
+    match part_two() {
+        Ok(msg) => println!("Two Success: {}", msg),
+        Err(err) => println!("Two Failed: {}", err),
     }
 }
 
 fn part_one() -> Result<String, String> {
     let ops = load_all_operations();
-    let input = load_part_one_input()?;
+    let input = load_samples()?;
     let mut samples_matching_three = 0;
 
     for item in input {
@@ -35,8 +40,58 @@ fn part_one() -> Result<String, String> {
         }
     }
 
-
     Ok(format!("{}", samples_matching_three))
+}
+
+fn part_two() -> Result<String, String> {
+    let program = load_program()?;
+    let op_codes = resolve_op_codes()?;
+
+    let mut registers = vec![0, 0, 0, 0];
+
+    for (op, a, b, c) in program {
+        op_codes[&op](&mut registers, a, b, c);
+    }
+
+    Ok(format!("{}", registers[0]))
+}
+
+fn resolve_op_codes() -> Result<HashMap<Param, &'static Fn(&mut Vec<Reg>, Param, Param, Param)>, String> {
+    let operations = load_all_operations();
+    let sample = load_samples()?;
+    let mut candidates: HashMap<Param, (HashSet<usize>, HashSet<usize>)> = HashMap::new();
+
+    for item in sample {
+        let (op_code, a, b, c) = item.args;
+        let (ref mut matches, ref mut rejected) = candidates
+            .entry(op_code)
+            .or_insert_with(|| (HashSet::new(), HashSet::new()));
+
+        for (i, op) in operations.iter().enumerate() {
+            let mut registers = item.reg_before.clone();
+            op(&mut registers, a, b, c);
+            if registers == item.reg_after {
+                if !rejected.contains(&i) {
+                    matches.insert(i);
+                }
+            } else {
+                matches.remove(&i);
+                rejected.insert(i);
+            }
+        }
+    }
+
+    for (i, (set, _)) in &candidates {
+        if set.len() != 1 {
+            return Err(format!("Didn't resolve to single op {}: {:?}", i, set));
+        }
+    }
+
+    let results: HashMap<_, _> = candidates.iter()
+        .map(|(k, (v, _))| (*k, operations[*v.iter().next().unwrap()]))
+        .collect();
+
+    Ok(results)
 }
 
 #[derive(Debug)]
@@ -46,7 +101,7 @@ struct PartOneInput {
     args: (Param, Param, Param, Param),
 }
 
-fn load_part_one_input() -> Result<Vec<PartOneInput>, String> {
+fn load_samples() -> Result<Vec<PartOneInput>, String> {
     let input = read_file_contents_as_string_from_path("before-after-opcodes.txt")?;
     let mut lines = input.lines();
     let mut results = Vec::new();
@@ -80,5 +135,28 @@ fn load_part_one_input() -> Result<Vec<PartOneInput>, String> {
         input[open + 1..close].split(",")
             .map(|s| Reg::from_str(s.trim()).map_err(|e| format!("Could not parse {}: {}", s, e)))
             .collect()
+    }
+}
+
+fn load_program() -> Result<Vec<(Param, Param, Param, Param)>, String> {
+    let input = read_file_contents_as_string_from_path("test-program.txt")?;
+    return input.lines()
+        .map(|line| {
+            let mut parts = line.split_whitespace();
+            let op = parse_param(parts.next())?;
+            let a = parse_param(parts.next())?;
+            let b = parse_param(parts.next())?;
+            let c = parse_param(parts.next())?;
+            Ok((op, a, b, c))
+        })
+        .collect::<Result<Vec<_>, _>>();
+
+    fn parse_param(value: Option<&str>) -> Result<Param, String> {
+        if let Some(v) = value {
+            Param::from_str(v)
+                .map_err(|e| format!("{}", e))
+        } else {
+            Err("Can not parse None as param".to_owned())
+        }
     }
 }
